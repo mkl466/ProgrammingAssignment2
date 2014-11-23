bc6469d034c9d1246d2f33e2f3e9b2170d5d650c
mkl466 <mkl466@hotmail.com>
2014-11-23 13:46:32 -0800

    Final check-in
    To verify:
    1. create matrix a
    2. run matrix multiplication to check the inverse validity: a%*%cacheSolve(makeCacheMatrix(a))

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
+#The first function, makeCacheMatrix creates a matrix containing a function to
+#1.set the value of the matrix
+#2.get the value of the matrix
+#3.set the inverse matrix using function solve(matrix)
+#4.get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-#test
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## The function attmpts to get inverse function from cache if available
+## Otherwice inverse is calculated
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
